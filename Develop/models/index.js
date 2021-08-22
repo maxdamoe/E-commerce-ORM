63d3dc6ec746bb3001fad885ae3e749a30f5b5fb
msimp <max.simpson@gmail.com>
2021-08-22 15:24:11 -0500

    Changes to server.js, config

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,32 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+/// THE FOLLOWING CODE BLOCKS STARTING HERE WERE PROVIDED IN CLASS BY MY INSTRUCTOR, GABE PERRY /////
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // foreignKey: "category_id",
+  // onDelete: "CASCADE"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  // foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+});
+
+/// THIS ENDS THE SECTION OF CODE PROVIDED IN CLASS BY MY INSTRUCTOR, GABE PERRY /////
+
 
 module.exports = {
   Product,
